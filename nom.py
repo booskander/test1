c0f5c86bb17b33136169926799299a04edf055a9
Skander <hannachiskander@live.ca>
2022-03-18 16:50:44 -0400

    yow

--- nom.py
+++ nom.py
@@ -1,2 +1,16 @@
 def imprimer():
 	print("Hello from py")
+	
+class MaClasse:
+	def __init__(self, a, b, c):
+		self.nom = a
+		self.age = b
+		self.matricule = c
+		
+	def afficher(self):
+		print(self.nom)
+		print(self.age)
+		print(self.matricule)
+		
+	def getNom(self):
+		return self.nom
